754eff04b6da72124c364386f11212b1505257ce
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-08-19 20:12:18 -0300

    Descobrindo a latitude e longitude

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
+import json
+import pprint
 
 r = requests.get('http://www.geoplugin.net/json.gp')
 
 if(r.status_code != 200):
     print('Não foi possível obter a localização!')
 else:
-    print(r.text) #Resposta da requisição
+    localizacao = json.loads(r.text) #Resposta da requisição transformada em JSON
+    latitude = localizacao['geoplugin_latitude']
+    longitude = localizacao['geoplugin_longitude']
+    print('lat:', latitude)
+    print('long:', longitude)
