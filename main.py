fe3eae982f2a511a5c7e825a7280d4128d006315
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-08-19 19:59:08 -0300

    Obtendo a localização do usuário

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+r = requests.get('http://www.geoplugin.net/json.gp')
+
+if(r.status_code != 200):
+    print('Não foi possível obter a localização!')
+else:
+    print(r.text) #Resposta da requisição
