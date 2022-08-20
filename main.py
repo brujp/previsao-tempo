ca35f5e6105d65f0a8d889248d4528489f6379e4
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-08-19 21:31:31 -0300

    Obtendo o código do local

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import pprint
+
+accuweatherApiKey = 'f2MFsAFv1sDdmoj6S9VRqeMC2tGChW4R'
 
 r = requests.get('http://www.geoplugin.net/json.gp')
 
@@ -9,5 +12,21 @@ else:
     localizacao = json.loads(r.text) #Resposta da requisição transformada em JSON
     latitude = localizacao['geoplugin_latitude']
     longitude = localizacao['geoplugin_longitude']
-    print('lat:', latitude)
-    print('long:', longitude)
+
+    #Requisição para pegar o código do local
+    locationApiUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuweatherApiKey + "&q=" + latitude + "%2C" + longitude + "&language=pt-br"
+    r2 = requests.get(locationApiUrl)
+
+    if (r2.status_code != 200):
+        print('Não foi possível obter o código do local!')
+    else:
+        locationResponse = json.loads(r2.text)
+        nomeLocal = locationResponse['LocalizedName'] + ", " \
+            + locationResponse['AdministrativeArea']['LocalizedName'] + ". " \
+            + locationResponse['Country']['LocalizedName']
+
+        codigoLocal = locationResponse['Key']
+
+        print("Local: " + nomeLocal)
+        print("Codigo: " + codigoLocal)
+
