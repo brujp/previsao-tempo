cdecfa4e81190c7fd0c5379027bd2933d3528292
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2022-08-22 21:21:56 -0300

    Refatoração

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 import requests
 import json
+# import pprint
 
 accuweatherApiKey = 'f2MFsAFv1sDdmoj6S9VRqeMC2tGChW4R'
 
-r = requests.get('http://www.geoplugin.net/json.gp')
+def pegarCoordenadas():
+    r = requests.get('http://www.geoplugin.net/json.gp')
 
-if(r.status_code != 200):
-    print('Não foi possível obter a localização!')
-else:
-    localizacao = json.loads(r.text) #Resposta da requisição transformada em JSON
-    latitude = localizacao['geoplugin_latitude']
-    longitude = localizacao['geoplugin_longitude']
+    if (r.status_code != 200):
+        print('Não foi possível obter a localização!')
+    else:
+        localizacao = json.loads(r.text)  # Resposta da requisição transformada em JSON
+        coordenadas = {}
+        coordenadas['latitude'] = localizacao['geoplugin_latitude']
+        coordenadas['longitude'] = localizacao['geoplugin_longitude']
+        return coordenadas  # Retorna um dicionário com a latitude e longitude
 
-    #Requisição para pegar o código do local
+def pegarCodigoDoLocal(latitude, longitude):
+    # API que retorna o código do local
     locationApiUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuweatherApiKey + "&q=" + latitude + "%2C" + longitude + "&language=pt-br"
-    r2 = requests.get(locationApiUrl)
+    r = requests.get(locationApiUrl)
 
-    if (r2.status_code != 200):
+    if (r.status_code != 200):
         print('Não foi possível obter o código do local!')
     else:
-        locationResponse = json.loads(r2.text)
-        nomeLocal = locationResponse['LocalizedName'] + ", " \
-            + locationResponse['AdministrativeArea']['LocalizedName'] + ". " \
-            + locationResponse['Country']['LocalizedName']
-        codigoLocal = locationResponse['Key']
+        locationResponse = json.loads(r.text)
+        infoLocal = {}
+        infoLocal['nomeLocal'] = locationResponse['LocalizedName'] + ", " \
+                                 + locationResponse['AdministrativeArea']['LocalizedName'] + ". " \
+                                 + locationResponse['Country']['LocalizedName']
+        infoLocal['codigoLocal'] = locationResponse['Key']
+        return infoLocal
 
-        print('Obtendo clima do local: ' + nomeLocal)
+def pegarTempoAgora(codigoLocal, nomeLocal):
+    # API que retorna a temperatura e o texto do clima
+    currentConditionsApiUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + codigoLocal + "?apikey=" + accuweatherApiKey + "&language=pt-br"
+    r = requests.get(currentConditionsApiUrl)
 
-        #API que retorna a temperatura e o texto do clima
-        currentConditionsApiUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + codigoLocal + "?apikey=" + accuweatherApiKey + "&language=pt-br"
-        r3 = requests.get(currentConditionsApiUrl)
+    if (r.status_code != 200):
+        print('Não foi possível obter a temperatura do local!')
+    else:
+        currentConditionsResponse = json.loads(r.text)
+        infoClima = {}
+        infoClima['textoClima'] = currentConditionsResponse[0]['WeatherText']
+        infoClima['temperatura'] = currentConditionsResponse[0]['Temperature']['Metric']['Value']
+        infoClima['nomeLocal'] = nomeLocal
+        return infoClima
 
-        if (r3.status_code != 200):
-            print('Não foi possível obter a temperatura do local!')
-        else:
-            currentConditionsResponse = json.loads(r3.text)
+# Inicio do programa
 
-        textoClima = currentConditionsResponse[0]['WeatherText']
-        temperatura = currentConditionsResponse[0]['Temperature']['Metric']['Value']
+coordenadas = pegarCoordenadas()
 
-        print('Clima no momento: ' + textoClima)
-        print('Temperatura: ' + str(temperatura) + ' oC')
+local = pegarCodigoDoLocal(coordenadas['latitude'], coordenadas['longitude'])
+
+climaAtual = pegarTempoAgora(local['codigoLocal'], local['nomeLocal'])
+
+print('Clima atual em: ' + climaAtual['nomeLocal'])
+print(climaAtual['textoClima'])
+print('Temperatura: ' + str(climaAtual['temperatura']) + ' oC')
